1d8fe3c5ad56ddad855e2fd55d16e558d5bc3b79
luizscoimbra <luizscoimbra@gmail.com>
2021-11-24 11:28:12 -0300

    Tela login

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/index", (req, res) => {
+  res.render("../views/index");
+});
+
+app.get("/cadastro", (req, res) => {
+  res.render("../views/cadastro");
+});
+
+app.listen(port, () =>
+  console.log(`Servidor rodando em http://localhost:${port}`)
+);
